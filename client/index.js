7744c42c1a94c3065e43a327498f8268be03f3e9
Jailany <jailany.m@zolostays.com>
2019-11-06 12:58:41 +0530

    Redux Setup and Split Template Implementation

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './source/App';
+import { configureStore } from '../server/framework/store';
 
-render(<App />, document.getElementById('root'));
+const initialState = window.__PRELOADED_STATE__
+  ? window.__PRELOADED_STATE__
+  : {};
+const store = configureStore(initialState);
+
+render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
--- server/framework/express.js
+++ server/framework/express.js
@@ -4,20 +4,45 @@ const device = require('express-device');
 const path = require('path');
 const fs = require('fs');
 
+import { configureStore } from './store';
+
 const port = 3000;
+
+// create an express app
 const app = express();
 
+// find the device type
 app.use(device.capture());
+
+// set the build folder as static for serving assets and other resources
 app.use(express.static('./build', { maxAge: 345600000 }));
 
+app.use((req, res, next) => {
+  // force tablets to desktop template
+  if (req.device.type == 'tablet') {
+    req.device.type = 'desktop';
+  }
 
-app.get('*', (req, res, next) => {
-  const htmlPath = path.resolve(__dirname, '../../build/main.html');
-  fs.readFile(htmlPath, 'utf8', (err, data) => {
-    res.send(data);
-  })
+  if (req.device.type == 'bot') {
+    req.device.type == 'mobile'
+  }
+  next();
 });
 
+// for any request, read the HTML file in the build folder and send it as response
+app.get('*', (req, res, next) => {
+
+  const store = configureStore({});
+  const state = store.getState();
+  state.core.config.deviceType = req.device.type;
+  const htmlPath = path.resolve(__dirname, '../../build/main.html');
+  fs.readFile(htmlPath, 'utf8', (err, data) => {
+    const html = data.replace('<script></script>', `<script>window.__PRELOADED_STATE__=${JSON.stringify(state)}</script>`)
+    res.send(html);
+  });
+});
+
+// start the server and listen on the given port
 app.listen(port, () => {
   console.log(`express server running on port - ${port}`);
 });
